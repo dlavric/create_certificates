8df20bfe30f81b39f3ba6d37d1fab43b863c6d44
Daniela Lavric <daniela@hashicorp.com>
2024-03-07 11:17:17 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,7 +13,9 @@ terraform {
 
 provider "acme" {
   # Configuration options
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  #it ceates untrusted certificates
+  #server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
 provider "aws" {
